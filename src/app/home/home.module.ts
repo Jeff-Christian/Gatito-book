4761dd745c5fb9515f959f40b4bc6c48197a4393
JeffChristian <jeffchristiansantos@gmail.com>
2022-07-09 17:53:29 -0300

    formulario de login para autenticação do usuário

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,13 @@ import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    LoginComponent,
   ],
   imports: [
     CommonModule,
