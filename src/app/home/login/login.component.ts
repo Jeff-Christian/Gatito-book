c039bc131f57494380794f79f438e859dedd0de4
JeffChristian <jeffchristiansantos@gmail.com>
2022-07-09 21:57:38 -0300

    conexção entre a API e a autenticação de usuário de nossa página de home

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,3 +1,4 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,17 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  usuario = '@';
+  usuario = '';
   senha = '';
 
-  constructor() { }
+  constructor(
+    private authService: AutenticacaoService,
+  ) { }
 
   ngOnInit(): void {
   }
 
   login(){
-    console.log(this.usuario);
-    console.log(this.senha);
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      () => {
+        console.log('Autenticado com sucesso');
+      }, (error)  => {
+        alert('usuario ou senha inválido');
+        console.log(error);
+      }
+    );
   }
 
 }
