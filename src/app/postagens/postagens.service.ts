e01f5ceffaf2a80b6278c2162615b38845bb7d9b
JeffChristian <jeffchristiansantos@gmail.com>
2022-07-14 18:52:23 -0300

    inserimos o serviço de postagens em nossa API, podendo requisitar e enviar para o modulo de postagens e cada usuário acessar suas postagens individuais

---
+++ src/app/postagens/postagens.service.ts
@@ -0,0 +1,27 @@
+import { TokenService } from './../autenticacao/token.service';
+import { Postagens } from './postagens';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+const API = 'http://localhost:3000';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostagensService {
+
+  constructor(
+    private httpClient: HttpClient,
+    private tokenService: TokenService,
+  ) { }
+
+  postsUser(nomeDoUsuario: string): Observable<Postagens>{
+    const token = this.tokenService.returnToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+    return this.httpClient.get<Postagens>(`${API}/${nomeDoUsuario}/photos`, {
+      headers,
+    });
+  }
+
+}
