f3942293092d653b4c898b6ef7b51bbb48b75405
JeffChristian <jeffchristiansantos@gmail.com>
2022-07-13 22:28:16 -0300

    ajuste no service de autenticacao para que não receba mais apenas observable e sim o response com a chave de token do usuário, o x-acess-token

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { UserService } from './user/user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,19 @@ export class AutenticacaoService {
 
   constructor(
     private httpClient: HttpClient,
+    private userService: UserService,
   ) { }
 
-  autenticar(usuario: string, senha: string): Observable<any>{
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>>{
     return this.httpClient.post('http://localhost:3000/user/login', {
       userName: usuario,
       password: senha,
-    });
+    },
+    { observe: 'response'}).pipe(
+      tap((res) => {
+        const authToken = res.headers.get('x-acess-token') ?? '';
+        this.userService.saveToken(authToken);
+      })
+    )
   }
 }
