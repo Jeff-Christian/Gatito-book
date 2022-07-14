c833974ed828dc29d6643dc57729660092a8d94e
JeffChristian <jeffchristiansantos@gmail.com>
2022-07-14 00:54:01 -0300

    autorização de entrada do usuario, informando novo e user alem da saída, cabeçalho e rodape padrao das páginas pra ajudar no direcionamento do usuário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { RouterModule } from '@angular/router';
+import { FooterModule } from './componentes/footer/footer.module';
+import { HeaderModule } from './componentes/header/header.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
@@ -12,6 +15,9 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    HeaderModule,
+    FooterModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
