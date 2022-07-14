ed5eea0d9dc09df058172a69638af2e0a6ed04c2
JeffChristian <jeffchristiansantos@gmail.com>
2022-07-14 19:21:26 -0300

    ajuste no nosso endereço da API no enviroment para que em produção esse endereço possa ser alterado em um único lugar ao invés de a cada arquivo de utilização da interpolação

--- src/app/home/create-user/new-user.service.ts
+++ src/app/home/create-user/new-user.service.ts
@@ -1,6 +1,9 @@
+import { environment } from './../../../environments/environment.prod';
 import { NewUser } from './new-user';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+const API = environment.apiURL;
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +15,11 @@ export class NewUserService {
   ) { }
 
   signUp(newUser: NewUser){
-    return this.http.post('http://localhost:3000/user/signup', newUser);
+    return this.http.post(`${API}/Uuser/signup`, newUser);
   }
 
   lookUserExist(nomeUsuario: string){
-    return this.http.get(`http://localhost:3000/user/exists/${nomeUsuario}`);
+    return this.http.get(`${API}/user/exists/${nomeUsuario}`);
   }
 
 }
